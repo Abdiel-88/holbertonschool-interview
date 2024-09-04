50d7d26caad7b4fee65ec89496b3db52fecf8db4
Abdiel Feliciano <abdiel.f@outlook.com>
2024-09-04 13:37:24 -0400

    fix code

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,25 +1,45 @@
 #!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially from 0 to n - 1
+and each box may contain keys to the other boxes.
+
+Write a method that determines if all the boxes can be opened.
+    Prototype: def canUnlockAll(boxes)
+    boxes is a list of lists
+    A key with the same number as a box opens that box
+    You can assume all keys will be positive integers
+    There can be keys that do not have boxes
+    The first box boxes[0] is unlocked
+    Return True if all boxes can be opened, else return False
+"""
+
+
 def canUnlockAll(boxes):
     """
     Determines if all boxes can be opened.
-    
-    Parameters:
-    boxes (list of lists): A list where each element is a list of keys available in that box.
-    
+
+    Args:
+        boxes (list of lists): A list where each element is a list of keys
+                                contained in a box. The box at index x has
+                                keys to other boxes.
+
     Returns:
-    bool: True if all boxes can be opened, else False.
+        bool: True if all boxes can be opened, False otherwise.
     """
-    n = len(boxes)
-    opened_boxes = [False] * n
-    opened_boxes[0] = True  # The first box is always open
-    keys = boxes[0]  # Start with keys from the first box
+    n = len(boxes)  # Gives us the total number of boxes.
+    opened = [False] * n  # Bool list that tracks which boxes have been opened
+    opened[0] = True  # True because this is the first box that is unlocked
+    stack = [0]  # Initializes the stack with the first box.
 
-    while keys:
-        new_keys = []
-        for key in keys:
-            if key < n and not opened_boxes[key]:
-                opened_boxes[key] = True
-                new_keys.extend(boxes[key])  # Add keys from the newly opened box
-        keys = new_keys  # Continue with the new set of keys
+    # To explore all of the keys found in the boxes.
+    while stack:
+        current_box = stack.pop()  # Assigns the last opened box from stack
+        for key in boxes[current_box]:  # Checks all of the keys in the box.
+            # Check if the key corresponds to a valid unopened box.
+            if 0 <= key < n and not opened[key]:
+                opened[key] = True  # Here we mark the box as opened.
+                stack.append(key)  # Here we add the box to the stack.
 
-    return all(opened_boxes)
+    # Return True if all of the boxes have been opened, otherwise False.
+    return all(opened)
